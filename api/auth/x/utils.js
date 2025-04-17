b904e382ea415d2c7be14fc0e3886b81c98b7637
AVAXCOCO <info@avaxcoco.com>
2025-04-17 12:19:09 -0500

    X auth fixes

--- api/auth/x/utils.js
+++ api/auth/x/utils.js
@@ -1,8 +1,8 @@
 // api/auth/x/utils.js
 function getRedirectUri(req) {
-    // Implementation of getRedirectUri function
-    // This is just a placeholder, you should replace it with the actual implementation
-    return 'https://example.com/api/auth/x/callback';
+    const host = req.headers.host;
+    const protocol = req.headers['x-forwarded-proto'] || 'http';
+    return `${protocol}://${host}/api/auth/x/callback`;
 }
 
 module.exports = { getRedirectUri };
