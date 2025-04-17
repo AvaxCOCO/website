a967ad900e6f81828af4090fafee5f18d7f8e84b
AVAXCOCO <info@avaxcoco.com>
2025-04-17 07:33:28 -0500

    added utils.js

---
+++ api/auth/x/utils.js
@@ -0,0 +1,8 @@
+// api/auth/x/utils.js
+function getRedirectUri(req) {
+    // Implementation of getRedirectUri function
+    // This is just a placeholder, you should replace it with the actual implementation
+    return 'https://example.com/api/auth/x/callback';
+}
+
+module.exports = { getRedirectUri };
